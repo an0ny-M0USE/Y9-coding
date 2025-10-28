7b0120d750b66b0415cdc9bbc813cb3b393e24be
an0ny-M0USE <elee29@ucc.on.ca>
2025-10-28 14:07:48 -0400

    Add files via upload

---
+++ example4.py
@@ -0,0 +1,46 @@
+# Coloured normal text
+
+print("\u001b[31m Colour!\u001b")
+print("\u001b[32m Colour!\u001b")
+print("\u001b[33m Colour!\u001b")
+print("\u001b[34m Colour!\u001b")
+print("\u001b[35m Colour!\u001b")
+print("\u001b[36m Colour!\u001b")
+print("\u001b[37m Colour!\u001b")
+
+# Coloured underlined normal text
+print("\u001b[4;31mColour!\u001b")
+print("\u001b[4;32mColour!\u001b")
+print("\u001b[4;33mColour!\u001b")
+print("\u001b[4;34mColour!\u001b")
+print("\u001b[4;35mColour!\u001b")
+print("\u001b[4;36mColour!\u001b")
+print("\u001b[4;37mColour!\u001b")
+
+# Coloured background
+print("\u001b[1;41m Colour!\u001b")
+print("\u001b[1;42m Colour!\u001b")
+print("\u001b[1;43m Colour!\u001b")
+print("\u001b[1;44m Colour!\u001b")
+print("\u001b[1;45m Colour!\u001b")
+print("\u001b[1;46m Colour!\u001b")
+print("\u001b[1;47m Colour!\u001b")
+
+# Bolded text
+print("\033[0;100m Colour!\033")
+print("\033[0;101m Colour!\033")
+print("\033[0;102m Colour!\033")
+print("\033[0;103m Colour!\033")
+print("\033[0;104m Colour!\033")
+print("\033[0;105m Colour!\033")
+print("\033[0;106m Colour!\033")
+print("\033[0;107m Colour!\033")
+
+# High Intensity Backgrounds
+print("\033[1;30m Colour!\033")
+print("\033[1;31m Colour!\033")
+print("\033[1;32m Colour!\033")
+print("\033[1;33m Colour!\033")
+print("\033[1;34m Colour!\033")
+print("\033[1;35m Colour!\033")
+print("\033[1;36m Colour!\033")
